d383cd5ff8a69bf4ca37812eab159528581d60ac
Anil <anil.shafeeque@gmail.com>
2019-10-23 20:41:19 +0300

    Updated peering

--- Hub1-Spoke1-Peering/main.tf
+++ Hub1-Spoke1-Peering/main.tf
@@ -3,6 +3,14 @@ provider azurerm {
     
 }
 
+locals {
+  vnetname1 = "qrhub1-vnet"
+  resourcegroupname1 = "qrhub1-rg"
+
+  vnetname2 = "qrspoke1-vnet"
+  resourcegroupname2 = "qrspoke1-rg"
+  
+}
 
 locals {
   common_tags = {
@@ -12,37 +20,52 @@ locals {
   }
 }
 
-resource "azurerm_subnet" "sub" {
-  name                      = "${var.Subnetname}"
-  resource_group_name       = "${var.resourcegroupname}"
-  virtual_network_name      = "${var.vnetname}"
-  address_prefix            = "10.0.2.0/27"
+data  "azurerm_resource_group" "vnet1"{
+
+name     = "${local.resourcegroupname1}"
+
+
+}
+data "azurerm_virtual_network" "vnet1" {
+  name                = "${local.vnetname1}"
+  resource_group_name = "${data.azurerm_resource_group.vnet1.name}"
+  #address_space       = ["10.3.0.0/16"]
+  # tags = "${merge( local.common_tags)}"
   
-  #network_security_group_id = "${azurerm_network_security_group.spoke.id}"
 }
 
-resource "azurerm_network_security_group" "sub" {
-  name                = "${var.NetworkSecurityGroup}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resourcegroupname}"
-  tags = "${merge( local.common_tags)}"
+data  "azurerm_resource_group" "vnet2"{
 
-  security_rule {
-    name                       = "UDP"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "10.189.3.0/24"
-    destination_address_prefix = "*"
-  }
+name     = "${local.resourcegroupname2}"
+
 
 }
-
-resource "azurerm_subnet_network_security_group_association" "sub" {
-  subnet_id                 = "${azurerm_subnet.sub.id}"
-  network_security_group_id = "${azurerm_network_security_group.sub.id}"
+data "azurerm_virtual_network" "vnet2" {
+  name                = "${local.vnetname2}"
+  resource_group_name = "${data.azurerm_resource_group.vnet2.name}"
+  #address_space       = ["10.4.0.0/16"]
+  # tags = "${merge( local.common_tags)}"
+  
 }
- 
+
+
+resource "azurerm_virtual_network_peering" "peer1" {
+  name                      = "peer1to2"
+  resource_group_name       = "${data.azurerm_resource_group.vnet1.name}"
+  virtual_network_name      = "${data.azurerm_virtual_network.vnet1.name}"
+  remote_virtual_network_id = "${data.azurerm_virtual_network.vnet2.id}"
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = true
+}
+
+resource "azurerm_virtual_network_peering" "peer2" {
+  name                      = "peer2to1"
+  resource_group_name       = "${data.azurerm_resource_group.vnet2.name}"
+  virtual_network_name      = "${data.azurerm_virtual_network.vnet2.name}"
+  remote_virtual_network_id = "${data.azurerm_virtual_network.vnet1.id}"
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = true
+}
