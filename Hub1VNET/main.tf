8c16111a77b8f948d2a4eb02d2b0fa7574d6e3ca
Anil Shafeeque <anil.shafeeque@gmail.com>
2019-10-28 20:14:25 +0300

    Update main.tf

--- Hub1VNET/main.tf
+++ Hub1VNET/main.tf
@@ -38,39 +38,3 @@ resource "azurerm_virtual_network" "vnet" {
   tags = "${merge( local.common_tags)}"
   
 }
-
-resource "azurerm_subnet" "vnet" {
-  name                      = "${var.Subnetname}"
-  resource_group_name       = "${azurerm_resource_group.vnet.name}"
-  virtual_network_name      = "${azurerm_virtual_network.vnet.name}"
-  address_prefix            = "10.3.0.0/24"
-  
-  #network_security_group_id = "${azurerm_network_security_group.spoke.id}"
-}
-
-
-resource "azurerm_network_security_group" "vnet" {
-  name                = "${var.NetworkSecurityGroup}"
-  location            = "${azurerm_resource_group.vnet.location}"
-  resource_group_name = "${azurerm_resource_group.vnet.name}"
-  tags = "${merge( local.common_tags)}"
-
-  security_rule {
-    name                       = "UDP"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "10.189.3.0/24"
-    destination_address_prefix = "*"
-  }
-
-}
-
-resource "azurerm_subnet_network_security_group_association" "vnet" {
-  subnet_id                 = "${azurerm_subnet.vnet.id}"
-  network_security_group_id = "${azurerm_network_security_group.vnet.id}"
-  
-}
